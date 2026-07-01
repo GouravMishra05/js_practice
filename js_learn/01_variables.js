bf36e9382aecbbca1b8afdc9c6c37c7e92b8bbc0
Gourav Mishra <gouravmishra125@gmail.com>
2026-07-01 06:52:50 +0000

    1st chapter learnt

---
+++ js_learn/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=222222
+let accountName="Gourav"
+var accountBalance= "1000.50"
+customerCity="New York"
+let accountState;
+
+// accountId=333333
+accountName="John Doe"
+accountBalance= "2000.75"
+customerCity="Los Angeles"
+
+console.table([accountId, accountName, accountBalance, customerCity, accountState])
+
+// prefer not to use var and use let to declare variables
